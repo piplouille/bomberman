ed52dd02c50d3ec2a4ff9eb6ecbde08dde710c73
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2019-12-17 12:19:10 +0100

    avancée du main pour tester déplacement et initialisation

--- basics/main.cpp
+++ basics/main.cpp
@@ -5,6 +5,11 @@ int main () {
     Player maxence(0);
     Map map;
 
+    // on va mettre maxence sur bloc (0, 0)
+
+    // il faut thread dédié en while(1)
+    // inline void Player::move(char move){
+
 
     return 0;
 }
