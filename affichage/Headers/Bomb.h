7d73c0b72c5c1714ff17bce31f36a46d826cc390
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-02-04 23:26:57 +0100

    Bomb.h fait

--- affichage/Headers/Bomb.h
+++ affichage/Headers/Bomb.h
@@ -1,3 +1,6 @@
+#ifndef bombHPP
+#define bombHPP
+
 #include <QObject>
 #include <QWidget>
 #include <QTimer>
@@ -13,13 +16,14 @@
 class Bloc;
 class Map;
 
-class Bomb : public QObject, public QGraphicsPixmapItem
-{
+class Bomb : public QObject, public QGraphicsPixmapItem {
+
     Q_OBJECT
+
 private:
     int posX, posY; //positions de la bombe
     int lifespan; //duree de vie de la premiere animation de la bombe
-    int power; //porté de la bombe
+    int range; // portée de la bombe
     int dist; // distance deja parcourue par la déflagration
     int state; // etat de l'animation
     QPixmap im_flashing_1;
@@ -34,22 +38,31 @@ private:
     QPixmap im_blast_2_left;
     QPixmap im_blast_3_left;
 
-    int range; // portée de la bombe
-    int life; // à chaque action, on enlève un
     std::shared_ptr<Player> owner; // permet de savoir quel décompte de bombe modifier à l'explosion
     std::vector<std::shared_ptr<Bloc> > touched; // l'ensemble des blocs que la bombe va atteindre, en 0 sa case
 
 
 public:
-    Bomb(int, int, int, int,QGraphicsPixmapItem* parent = nullptr);
+    Bomb(QGraphicsPixmapItem* parent = nullptr);
+    Bomb(Player&, QGraphicsPixmapItem* parent = nullptr);
+    Bomb(int, int, int, int, QGraphicsPixmapItem* parent = nullptr);
 
     ~Bomb(){};
 
+    int get_lifespan() {return lifespan;}
+    void decrease_lifespan() {lifespan--;}
 
+    void set_x(int x) {this->x = x;}
+    void set_y(int y) {this->y = y;}
 
 public slots:
-    void blast();
     void flashing();
+    void blast();
     void exploding();
     //void keyPressEvent(QKeyEvent *event);
 };
+
+#include "Bloc.hpp"
+#include "Map.hpp"
+
+#endif
--- affichage/Sources/Bomb.cpp
+++ affichage/Sources/Bomb.cpp
@@ -1,7 +1,83 @@
 #include "Headers/Bomb.h"
 #include <QTimer>
+/*
+Constructeurs
+*/
 
-Bomb::Bomb(int x, int y, int lifespan, int power,QGraphicsPixmapItem* parent): QGraphicsPixmapItem (parent) {
+Bomb::Bomb(QGraphicsPixmapItem* parent) : QGraphicsPixmapItem (parent) {
+    im_flashing_1 = QPixmap(":/Resources/Classic_bomb/Bomb_Flashing_1.png");
+    im_flashing_1 = im_flashing_1.scaled(QSize(24,24),Qt::KeepAspectRatio);
+    im_flashing_2 = QPixmap(":/Resources/Classic_bomb/Bomb_Flashing_2.png");
+    im_flashing_2 = im_flashing_2.scaled(QSize(24,24),Qt::KeepAspectRatio);
+    im_exploding_1 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_1.png");
+    im_exploding_1 = im_exploding_1.scaled(QSize(24,24),Qt::KeepAspectRatio);
+    im_exploding_2 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_2.png");
+    im_exploding_2 = im_exploding_2.scaled(QSize(24,24),Qt::KeepAspectRatio);
+    im_exploding_3 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_3.png");
+    im_exploding_3 = im_exploding_3.scaled(QSize(24,24),Qt::KeepAspectRatio);
+    im_exploding_4 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_4.png");
+    im_exploding_4 = im_exploding_4.scaled(QSize(24,24),Qt::KeepAspectRatio);
+    im_exploding_5 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_5.png");
+    im_exploding_5 = im_exploding_5.scaled(QSize(24,24),Qt::KeepAspectRatio);
+    im_exploding_6 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_6.png");
+    im_exploding_6 = im_exploding_6.scaled(QSize(24,24),Qt::KeepAspectRatio);
+    im_blast_1 = QPixmap(":/Resources/Classic_bomb/blast_1.png");
+    im_blast_1 = im_blast_1.scaled(QSize(24,24),Qt::KeepAspectRatio);
+    im_blast_2_left = QPixmap(":/Resources/Classic_bomb/blast_2.png");
+    im_blast_2_left = im_blast_2_left.scaled(QSize(24,24),Qt::KeepAspectRatio);
+    im_blast_3_left = QPixmap(":/Resources/Classic_bomb/blast_3.png");
+    im_blast_3_left = im_blast_2_left.scaled(QSize(24,24),Qt::KeepAspectRatio);
+
+    dist = 0;
+    state = 0;
+
+    setPixmap(im_flashing_1);
+    QTimer::singleShot(1000, this, SLOT(flashing()));
+}
+
+
+Bomb::Bomb(Player& p, QGraphicsPixmapItem* parent) : Bomb(parent) {
+    if (p.able_bomb()) {
+        // On récupère les infos de joueur pour créer la bombe
+        posX = p.get_x();
+        posY = p.get_y();
+        range = p.get_bomb_range();
+        owner = std::make_shared<Player> (p); // permet de savoir quel décompte de bombe modifier à l'explosion
+        lifespan = p.get_bomb_life(); // à chaque tour ça décroit de 1
+
+        p.decrease_bomb_quota();
+
+        setPos(posX, posY);
+    }
+}
+
+Bomb::Bomb(Player& p, Map& map, QGraphicsPixmapItem* parent) : Bomb(p, parent) {
+    if (p.able_bomb()) {
+        // On ajoute les pointeurs vers les blocs qui seront touchés
+        int width = map.get_width();
+        int length = map.get_length();
+
+        touched.push_back(std::make_shared<Bloc> (*map.begin(posX, posY)));
+        
+        for (int indice = 1; indice < range; indice ++) {
+            if ((posX-indice) >= 0) {
+                touched.push_back(std::make_shared<Bloc> (*map.begin(posX-indice, posY)));
+            }
+            if ((posX+indice) < width) {
+                touched.push_back(std::make_shared<Bloc> (*map.begin(posX+indice, posY)));
+            }
+            if ((posY-indice) >= 0) {
+                touched.push_back(std::make_shared<Bloc> (*map.begin(posX, posY-indice)));
+            }
+            if ((posY+indice) < length) {
+                touched.push_back(std::make_shared<Bloc> (*map.begin(posX, posY+indice)));
+            }            
+        }
+    }
+}
+
+
+Bomb::Bomb(int x, int y, int lifespan, int power, QGraphicsPixmapItem* parent): QGraphicsPixmapItem (parent) {
     im_flashing_1 = QPixmap(":/Resources/Classic_bomb/Bomb_Flashing_1.png");
     im_flashing_1 = im_flashing_1.scaled(QSize(24,24),Qt::KeepAspectRatio);
     im_flashing_2 = QPixmap(":/Resources/Classic_bomb/Bomb_Flashing_2.png");
@@ -30,19 +106,47 @@ Bomb::Bomb(int x, int y, int lifespan, int power,QGraphicsPixmapItem* parent): Q
     this -> setPixmap(im_flashing_1);
     setPos(posX, posY);
     this->lifespan = lifespan;
-    this->power = power;
+    range = power;
     dist = 0;
     state = 0;
     //flashing(lifespan);
     QTimer::singleShot(1000, this, SLOT(flashing()));
 }
 
+/*
+Affichage avant explosion
+*/
+
+void Bomb::flashing() {
+    lifespan -= 50;
+    if(lifespan == 0) {
+        state = 1;
+        QTimer::singleShot(25, this, SLOT(exploding()));
+    }
+    else {
+        if(state==0) {
+            setPixmap(im_flashing_2);
+            state ++;
+        }
+        else {
+            setPixmap(im_flashing_1); 
+            state--;
+        }
+        QTimer::singleShot(lifespan,this,SLOT(flashing()));
+    }
+    return;
+}
+
+/*
+Explosion
+*/
+
 void Bomb::blast() {
     //à chaque occurence on crée un QGraphicsPixmapItem que l'on positionne selon posX et posY
     //à la fin, ils sont tous enlevés et détruits
     /*option 2 : une boucle while auto suffisante */
     dist++;
-    if(dist<power) {
+    if(dist<range) {
         // create image objects that shall be destroyed
         QGraphicsPixmapItem *blaL = new QGraphicsPixmapItem(im_blast_2_left);
         QGraphicsPixmapItem *blaR = new QGraphicsPixmapItem(im_blast_2_left);
@@ -71,7 +175,7 @@ void Bomb::blast() {
         delete blaU;
         delete blaD;
     }
-    else if(dist==power) {
+    else if(dist==range) {
         // create image objects that shall be destroyed
         QGraphicsPixmapItem *blaL = new QGraphicsPixmapItem(im_blast_3_left);
         QGraphicsPixmapItem *blaR = new QGraphicsPixmapItem(im_blast_3_left);
@@ -103,25 +207,6 @@ void Bomb::blast() {
     }
 }
 
-void Bomb::flashing() {
-    lifespan -= 50;
-    if(lifespan == 0) {
-        state = 1;
-        QTimer::singleShot(25, this, SLOT(exploding()));
-    }
-    else {
-        if(state==0) {
-            setPixmap(im_flashing_2);
-            state ++;
-        }
-        else {
-            setPixmap(im_flashing_1); 
-            state--;
-        }
-        QTimer::singleShot(lifespan,this,SLOT(flashing()));
-    }
-    return;
-}
 
 void Bomb::exploding() {
     state++;
