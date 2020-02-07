c0c62dd9c2cfb22cc5b55626adbb32b41c54a6e6
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-02-07 19:57:28 +0100

    sa march

--- affichage/moc_player.cpp
+++ affichage/moc_player.cpp
@@ -1,19 +1,18 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'player.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.0)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.5)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include <memory>
 #include "Headers/player.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'player.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.14.0. It"
+#error "This file was generated using the moc from 5.9.5. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -44,7 +43,7 @@ QT_MOC_LITERAL(2, 16, 0) // ""
 static const uint qt_meta_data_Player[] = {
 
  // content:
-       8,       // revision
+       7,       // revision
        0,       // classname
        0,    0, // classinfo
        1,   14, // methods
@@ -66,7 +65,7 @@ static const uint qt_meta_data_Player[] = {
 void Player::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
-        auto *_t = static_cast<Player *>(_o);
+        Player *_t = static_cast<Player *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->desepear(); break;
@@ -76,14 +75,10 @@ void Player::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
     Q_UNUSED(_a);
 }
 
-QT_INIT_METAOBJECT const QMetaObject Player::staticMetaObject = { {
-    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
-    qt_meta_stringdata_Player.data,
-    qt_meta_data_Player,
-    qt_static_metacall,
-    nullptr,
-    nullptr
-} };
+const QMetaObject Player::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_Player.data,
+      qt_meta_data_Player,  qt_static_metacall, nullptr, nullptr}
+};
 
 
 const QMetaObject *Player::metaObject() const
