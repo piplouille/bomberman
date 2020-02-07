aadabd5f869de178561d4d927c6cc88f4e79390c
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-02-07 22:57:30 +0100

    Bomb cleanified

--- affichage/Sources/Bomb.cpp
+++ affichage/Sources/Bomb.cpp
@@ -2,6 +2,95 @@
 #include <QTimer>
 #include <QDebug>
 
+Bomb::Bomb(char type, int x, int y, int lifespan, int power, Player* joueur1, Player* joueur2 ,
+    Player* joueur3 ,Player* joueur4 ,QGraphicsPixmapItem* parent) :
+    QGraphicsPixmapItem (parent) {
+    
+    this->joueur1 = joueur1;
+    this->joueur2 = joueur2;
+    this->joueur3 = joueur3;
+    this->joueur4 = joueur4;
+
+    size = 32;
+    if(type == 'C') {
+        im_flashing_1 = QPixmap(":/Resources/Classic_bomb/Bomb_Flashing_1.png");
+        im_flashing_2 = QPixmap(":/Resources/Classic_bomb/Bomb_Flashing_2.png");
+        im_exploding_1 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_1.png");
+        im_exploding_2 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_2.png");
+        im_exploding_3 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_3.png");
+        im_exploding_4 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_4.png");
+        im_exploding_5 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_5.png");
+        im_exploding_6 = QPixmap(":/Resources/Classic_bomb/Bomb_Exploding_6.png");
+        im_blast_1 = QPixmap(":/Resources/Classic_bomb/blast_2.png");
+        im_blast_2_left = QPixmap(":/Resources/Classic_bomb/blast_1.png");
+        im_blast_3_left = QPixmap(":/Resources/Classic_bomb/blast_3.png");
+    }
+
+    else if(type == 'B') {
+        im_flashing_1 = QPixmap(":/Resources/bombitrouille/BBT_flashing_1.png");
+        im_flashing_2 = QPixmap(":/Resources/bombitrouille/BBT_flashing_2.png");
+        im_exploding_1 = QPixmap(":/Resources/bombitrouille/BBT_Exp_1.png");
+        im_exploding_2 = QPixmap(":/Resources/bombitrouille/BBT_Exp_2.png");
+        im_exploding_3 = QPixmap(":/Resources/bombitrouille/BBT_Exp_3.png");
+        im_exploding_4 = QPixmap(":/Resources/bombitrouille/BBT_Exp_4.png");
+        im_exploding_5 = QPixmap(":/Resources/bombitrouille/BBT_Exp_5.png");
+        im_exploding_6 = QPixmap(":/Resources/bombitrouille/BBT_Exp_6.png");
+        im_blast_1 = QPixmap(":/Resources/bombitrouille/BBT_Blast_1.png");
+        im_blast_2_left = QPixmap(":/Resources/bombitrouille/BBT_Blast_2.png");
+        im_blast_3_left = QPixmap(":/Resources/bombitrouille/BBT_Blast_3.png");
+    }
+
+    im_flashing_1 = im_flashing_1.scaled(QSize(size,size),Qt::KeepAspectRatio);
+    im_flashing_2 = im_flashing_2.scaled(QSize(size,size),Qt::KeepAspectRatio);
+    im_exploding_1 = im_exploding_1.scaled(QSize(size,size),Qt::KeepAspectRatio);
+    im_exploding_2 = im_exploding_2.scaled(QSize(size,size),Qt::KeepAspectRatio);
+    im_exploding_3 = im_exploding_3.scaled(QSize(size,size),Qt::KeepAspectRatio);
+    im_exploding_4 = im_exploding_4.scaled(QSize(size,size),Qt::KeepAspectRatio);
+    im_exploding_5 = im_exploding_5.scaled(QSize(size,size),Qt::KeepAspectRatio);
+    im_exploding_6 = im_exploding_6.scaled(QSize(size,size),Qt::KeepAspectRatio);
+    im_blast_1 = im_blast_1.scaled(QSize(size,size),Qt::KeepAspectRatio);
+    im_blast_2_left = im_blast_2_left.scaled(QSize(size,size),Qt::KeepAspectRatio);
+    im_blast_3_left = im_blast_3_left.scaled(QSize(size,size),Qt::KeepAspectRatio);
+
+    this -> posX = x;
+    this -> posY = y;
+    this -> setPixmap(im_flashing_1);
+    setPos(posX,posY);
+    this->lifespan = lifespan;
+    this->power = power;
+    dist = 0;
+    state = 0;
+    QTimer::singleShot(1000, this, SLOT(flashing()));
+}
+
+/*
+Affichage bombe
+*/
+
+void Bomb::flashing() {
+    lifespan -= 30;
+    if(lifespan <= 0) {
+        state = 1;
+        QTimer::singleShot(25, this, SLOT(exploding()));
+    }
+    else {
+        if(state==0) {
+            setPixmap(im_flashing_2);
+            state ++;
+        }
+        else {
+            setPixmap(im_flashing_1); 
+            state--;
+        }
+        QTimer::singleShot(lifespan,this,SLOT(flashing()));
+    }
+    return;
+}
+
+/*
+Explosion
+*/
+
 void Bomb::blast() {
 
     dist++;
@@ -79,25 +168,6 @@ void Bomb::blast() {
     // TODO trouver comment afficher les enfants
 }
 
-void Bomb::flashing() {
-    lifespan -= 30;
-    if(lifespan <= 0) {
-        state = 1;
-        QTimer::singleShot(25, this, SLOT(exploding()));
-    }
-    else {
-        if(state==0) {
-            setPixmap(im_flashing_2);
-            state ++;
-        }
-        else {
-            setPixmap(im_flashing_1); 
-            state--;
-        }
-        QTimer::singleShot(lifespan,this,SLOT(flashing()));
-    }
-    return;
-}
 
 void Bomb::exploding() {
     state++;
